fbd4321a44fe5b1d1d7c7364eaaafdba167fb4ed
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-07 18:13:07 +0000

    Initial commit

---
+++ scrip.js
@@ -0,0 +1,9 @@
+/*let num =prompt("Enter your number here")
+num = Number.parseInt(num)
+
+if(num >6){
+  location.href="https://google.com"
+}
+*/
+let col = prompt("Enter your color to see this page color ")
+document.body.style.background = col
---
+++ script.js
@@ -0,0 +1,21 @@
+
+/*let agenum = true;
+const driv = (age)=>{
+  return age>=18?true:false
+}
+  while(agenum){
+  let age = prompt("enter your age here -")
+age = Number.parseInt(age)
+if(age <0){
+      console.error("please enter a valid age ");
+  break
+    }
+
+if(driv(age)){
+  alert("you can drive")
+}
+else {
+  alert("you cannot drive")
+}
+  agenum = confirm("do you want to fil the form again?")
+}*/
